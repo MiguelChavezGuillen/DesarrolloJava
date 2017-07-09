47ccb16fa53c0e1ce7a28f666ed25cb75fb0d38a
MiguelChavezGuillen <1221537@utp.edu.pe>
2017-07-08 22:41:20 -0500

    Initial Commit

---
+++ src/pe/com/prueba/model/StudentActivity.java
@@ -0,0 +1,40 @@
+package pe.com.prueba.model;
+
+/**
+ * Created by Fjorsvartnir on 8/07/2017.
+ */
+public class StudentActivity {
+    private int id;
+    private Activity activity;
+    private Student student;
+
+    public StudentActivity(int id, Activity activity, Student student) {
+        this.id = id;
+        this.activity = activity;
+        this.student = student;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Activity getActivity() {
+        return activity;
+    }
+
+    public void setActivity(Activity activity) {
+        this.activity = activity;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+}
